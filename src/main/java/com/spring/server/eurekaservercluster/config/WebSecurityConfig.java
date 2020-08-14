e7e558352f79d8bd13ab05a048202dfb9f4fb737
吴开通 <87641487@qq.com>
2020-08-14 16:17:24 +0800

    Initial commit

---
+++ src/main/java/com/spring/server/eurekaservercluster/config/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.spring.server.eurekaservercluster.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @Description TODO
+ * @Author xianfeng
+ * @Date 2020/8/12 20:41
+ * @Version 1.0
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 关闭csrf
+        http.csrf().disable();
+        // 支持httpBasic
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+}
---
+++ src/test/java/com/spring/server/eurekaservercluster/EurekaServerClusterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.server.eurekaservercluster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerClusterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
